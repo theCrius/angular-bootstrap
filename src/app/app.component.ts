b2c1e20380bea8c8d61762ed4b7505cb0cb9d169
theCrius <claudio.vallesi@gmail.com>
2018-03-17 23:37:48 +0000

    Include momentJS and example of how to use it in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title: string;
+  timestamp: moment.Moment;
 }
